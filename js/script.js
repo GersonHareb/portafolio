8bd598a869775b514fabaddd39ef0cac53822801
Gerson Hareb Herrera Ruminot <gersonhareb@hotmail.com>
2023-08-09 12:53:37 -0400

    introduccion e implementacion de diseños para la barra nav

--- js/script.js
+++ js/script.js
@@ -26,3 +26,66 @@ const stars = document.querySelectorAll(".star");
 stars.forEach((star, index) => {
     star.style.animationDelay = `${index * 0.1}s`;
 });
+
+/*Animacion navbar*/
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.getElementById('custom-navbar');
+
+    function updateNavbarPosition() {
+        if (window.scrollY >= navbar.offsetTop) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+
+        // Volver a la posición original en la mitad de la pantalla de la sección de inicio
+        const windowHeight = window.innerHeight;
+        const halfWindowHeight = windowHeight / 2;
+        const sectionInicio = document.getElementById('welcome');
+        const sectionInicioTop = sectionInicio.offsetTop;
+
+        if (window.scrollY >= sectionInicioTop && window.scrollY < sectionInicioTop + halfWindowHeight) {
+            navbar.classList.remove('scrolled');
+        }
+
+        // Verificar si el scroll está sobre la sección "proyectos"
+        const sectionProyectos = document.getElementById('proyectos');
+        const sectionProyectosTop = sectionProyectos.offsetTop;
+        const sectionProyectosBottom = sectionProyectosTop + sectionProyectos.clientHeight;
+
+        if (window.scrollY >= sectionProyectosTop && window.scrollY < sectionProyectosBottom) {
+            navbar.classList.add('proyecto-nav');
+        } else {
+            navbar.classList.remove('proyecto-nav');
+        }
+
+        // Verificar si el scroll está sobre la sección "proyectos"
+        const sectionSobreMi= document.getElementById('sobre-mi');
+        const sectionSobreMiTop = sectionSobreMi.offsetTop;
+        const sectionSobreMiBottom = sectionSobreMiTop + sectionSobreMi.clientHeight;
+
+        if (window.scrollY >= sectionSobreMiTop && window.scrollY < sectionSobreMiBottom) {
+            navbar.classList.add('sobre-mi-nav');
+        } else {
+            navbar.classList.remove('sobre-mi-nav');
+        }
+
+        // Verificar si el scroll está sobre la sección "proyectos"
+        const sectionContacto = document.getElementById('contacto');
+        const sectionContactoTop = sectionContacto.offsetTop;
+        const sectionContactoBottom = sectionContactoTop + sectionContacto.clientHeight;
+
+        if (window.scrollY >= sectionContactoTop && window.scrollY < sectionContactoBottom) {
+            navbar.classList.add('contacto-nav');
+        } else {
+            navbar.classList.remove('contacto-nav');
+        }
+    } 
+
+    window.addEventListener('scroll', updateNavbarPosition);
+});
+
+  
+  
+  
+  
